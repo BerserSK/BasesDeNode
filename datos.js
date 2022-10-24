471d34df1cef69c47986503e5094558e14093f35
BerserSK <andres2003latorre@gmail.com>
2022-10-23 21:46:31 -0500

    Programa terminado

--- datos.js
+++ datos.js
@@ -10,15 +10,18 @@ let status = "Soltero :)"
 let address = "Calle 13"
 let phone = "3114748053"
 let email = "Wa@misena.eudo.con"
-let text = `
-              |     Soy una persona dinámica y creativa, habituada
-              |     al trabajo bajo presión. Tengo experiencia en atención
-              |     al usuario y muy buenas relaciones interpersonales, 
-              |     por lo cual se me facilita el trabajo en equipo, 
-              |     lo cual además disfruto.`
-
+let text=`Soy una persona dinámica y creativa, habituada al     |
+              |      trabajo bajo presión. Tengo experiencia en atención   | 
+              |      al usuario y buenas relaciones interpersonales, por   | 
+              |      lo cual  se me facilita el trabajo en equipo, lo cual |
+              |      además disfruto.                                      |`
+let school = "Estanislao Zuleta "
+let uni = "xxxxx"
 const hoja = `
-              |------------------------------------------------------------|
+              ______________________________________________________________
+              |                                                            |
+              |                                                            |
+              |                                                            |
               |                      HOJA DE VIDA                          |
               |                                                            |
               |                     ______________                         |
@@ -32,31 +35,34 @@ const hoja = `
               |                                                            |
               |      Nombres y Apellidos: ${nombre} ${apellido}                  |
               |                                                            |
-              |      Lugar y fecha de nacimiento: ${date}                  | 
-              |      
-              |      Cedula de Ciudadania: ${cedula}                       |             
-              |       
-              |      Sexo: ${sex}                                          |
-              |         
-              |      Estado Civil: ${status}                               |
-              |
-              |      Dirreccion: ${address}                                |
-              |
-              |      Telefono : ${phone}                                   |
-              |      
-              |      E-Mail: ${email}                                      |
-              |
-              |                         PERFIL PROFESIONAL
-              |
-              |      ${text}
-              |
-              |`
+              |      Lugar y fecha de nacimiento: ${date}               | 
+              |                                                            |
+              |      Cedula de Ciudadania: ${cedula}                      |             
+              |                                                            |
+              |      Sexo: ${sex}                                         |
+              |                                                            |
+              |      Estado Civil: ${status}                              |
+              |                                                            |
+              |      Dirreccion: ${address}                                  |
+              |                                                            |
+              |      Telefono : ${phone}                                 |
+              |                                                            |
+              |      E-Mail: ${email}                            |
+              |                                                            |
+              |                       PERFIL PROFESIONAL                   |
+              |                                                            |
+              |      ${text}                                               |
+              |                                                            |
+              |                       FORMACION ACADEMICA                  |
+              |                                                            | 
+              |     Colegio: ${school}| Bachiller Academico       |
+              |     2011                                                   |
+              |                                                            | 
+              |     Universidad : ${uni}| Pregrado en XXX                   |
+              |     2016                                                   |
+              |                                                            |
+              |____________________________________________________________|`
               
-
-
-
-
-
 
 fs.writeFile(`Informacion-${base}.txt`, hoja, (err)=>{
     if(err){
@@ -64,3 +70,5 @@ fs.writeFile(`Informacion-${base}.txt`, hoja, (err)=>{
         console.log(`Tabla-${base}.txt creado`);
     }
 });
+
+console.log(hoja)
